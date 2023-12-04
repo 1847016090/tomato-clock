e9b154ce95ec24f671a76465e6702bf09f93f4b0
yekang <1847016090@qq.com>
2023-12-04 11:28:59 +0800

    fix: 调整配置

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { SuperProvider } from "@rokid-library/components";
 import App from "./App";
 import "@/assets/style/commonClassNames.less";
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <SuperProvider
-    theme={{
-      token: {
-        colorPrimary: "#6213FF",
-        colorError: "#FF5353",
-        colorWarning: "#FFCC40",
-        colorSuccess: "#48DE5B",
-      },
-    }}
-  >
-    <App />
-  </SuperProvider>
+  <App />
 );
